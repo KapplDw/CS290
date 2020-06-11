38aa2711fb5cb485af165f9185edeca00d7c5f1c
KapplDw <55997890+KapplDw@users.noreply.github.com>
2020-06-11 14:05:42 -0700

    Basic button events added

--- public/index.js
+++ public/index.js
@@ -1,22 +1,32 @@
 // Adds event listener to sign in button
+window.addEventListener('DOMContentLoaded', function () {
+    var menuItems_delete = document.getElementsByClassName('menu_item');
 
-var menuItems_delete = document.getElementsByClassName('menu_item');
-console.log(menuItems_delete);
-console.log(menuItems_delete.length);
-for (var i = 0; i < menuItems_delete.length; i++) {
-    menuItems_delete.addEventListener('click', removeItem);
-    console.log("Added Event listener")
-};
+    for (var i = 0; i < menuItems_delete.length; i++) {
+        menuItems_delete[i].addEventListener('click', removeItem);
+    };
+
+    var addItem = document.getElementById('create-item');
+    addItem.addEventListener('click', showAddModal);  
+    
+    var signIn = document.getElementsByClassName('sign');
+    signIn.addEventListener('click', showButtons);
+});
+
 
 
 
 // Displays add/rm menu item button
+function showButtons(event){
 
+};
 
 // Updates the page and back end data
 
 // Adds event listener on add button 
+function showAddModal(event){
 
+};
 
 
 // Adds event listener on rm button
